63684dbfef1c6531aebf27bdf888f46652133bfd
18911956809@163.com <18911956809@163.com>
2016-11-03 15:25:14 +0800

    新增加user

---
+++ src/com/lx/User.java
@@ -0,0 +1,47 @@
+package com.lx;
+
+public class User {
+	
+	private int id;
+	
+	private String userName;
+	
+    private String password;
+    
+    private String nickName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+    
+    
+
+}
